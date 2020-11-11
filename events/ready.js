084ef464360fcfd946fd7b5313e04ab1140850bb
NWJ9PB <inspiregaming1025@gmail.com>
2020-11-11 14:25:44 +0800

    Made Different presence than server url

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 const sql = require('sqlite')
 sql.open('./users.sqlite')
 const settings = require('../settings.json');
-module.exports = async (client) => {
+module.exports = async(client) => {
     client.logger.log("to serve you!", "ready");
-    client.user.setPresence({ activity: { name: `with ${settings.panelURL}` }, status: 'online' })
+    client.user.setPresence({ activity: { name: `with ${settings.servercluster}` }, status: 'online' })
     sql.run(`CREATE TABLE IF NOT EXISTS users (id INTEGER, token TEXT)`);
 };
